c42a7f9204c31de83b2961cefaca1956f6ae47c2
huumanda <78459051+huumanda@users.noreply.github.com>
2021-03-04 12:30:35 -0500

    commit

--- shroomLibrary/shroomLibrary.js
+++ shroomLibrary/shroomLibrary.js
@@ -1,1 +1,4 @@
 // arrows: https://www.youtube.com/watch?v=bDYWUGCgKCI&ab_channel=TraversyMedia
+// text reveal animation: https://codepen.io/daviddarnes/pen/hdmjb
+// background clip: https://www.w3schools.com/cssref/css3_pr_background-clip.asp
+// border image: https://www.w3schools.com/cssref/css3_pr_border-image.asp

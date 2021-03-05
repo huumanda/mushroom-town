d080a2342ec56307f0b8bb145b0c119f912fc2b1
huumanda <78459051+huumanda@users.noreply.github.com>
2021-03-05 01:12:24 -0500

    commit

--- shroomLibrary/shroomLibrary.js
+++ shroomLibrary/shroomLibrary.js
@@ -2,3 +2,5 @@
 // text reveal animation: https://codepen.io/daviddarnes/pen/hdmjb
 // background clip: https://www.w3schools.com/cssref/css3_pr_background-clip.asp
 // border image: https://www.w3schools.com/cssref/css3_pr_border-image.asp
+// border radius tool: https://9elements.github.io/fancy-border-radius/ 
+// background responsive design: https://www.webfx.com/blog/web-design/responsive-background-image/

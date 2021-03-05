c452b42d31a82457c02d471970b8ffdb91c8c2bf
huumanda <78459051+huumanda@users.noreply.github.com>
2021-03-05 02:42:08 -0500

    commit

---
+++ shroomMisc/shroomMisc.js
@@ -0,0 +1,45 @@
+const myFunction = () => {
+    document.getElementById("i1").style.display ='block';
+    document.getElementById("i2").style.display ='none'
+    document.getElementById("i3").style.display ='none'
+    document.getElementById("i4").style.display ='none'
+    document.getElementById("i5").style.display ='none'
+
+  }
+  
+  const myFunction2 = () => {
+    document.getElementById("i2").style.display ='block'
+    document.getElementById("i1").style.display ='none'
+    document.getElementById("i3").style.display ='none'
+    document.getElementById("i4").style.display ='none'
+    document.getElementById("i5").style.display ='none'
+
+
+  }
+  
+  const myFunction3 = () => {
+    document.getElementById("i3").style.display ='block'
+    document.getElementById("i1").style.display ='none'
+    document.getElementById("i2").style.display ='none'
+    document.getElementById("i4").style.display ='none'
+    document.getElementById("i5").style.display ='none'
+
+  }
+  
+
+  const myFunction4 = () => {
+    document.getElementById("i4").style.display ='block'
+    document.getElementById("i1").style.display ='none'
+    document.getElementById("i2").style.display ='none'
+    document.getElementById("i3").style.display ='none'
+    document.getElementById("i5").style.display ='none'
+  }
+
+  const myFunction5 = () => {
+    document.getElementById("i5").style.display ='block'
+    document.getElementById("i1").style.display ='none'
+    document.getElementById("i2").style.display ='none'
+    document.getElementById("i3").style.display ='none'
+    document.getElementById("i4").style.display ='none'
+  }
+  
